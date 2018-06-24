fb292e94111d59d79a88ff045f2acbad80dd917d
songweishuai <songweishuai@thunder.com.cn>
2018-06-24 23:09:56 +0800

    实现问答切换

--- app/src/main/java/com/sws/android/quizactivity/QuizActivity.java
+++ app/src/main/java/com/sws/android/quizactivity/QuizActivity.java
@@ -4,12 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueBurron;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex=0;
+    private Question[] mQuestionBank={
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +36,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                //Toast.makeText(QuizActivity.this,"true",Toast.LENGTH_SHORT).show();
-                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
@@ -30,8 +45,34 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(QuizActivity.this,"true",Toast.LENGTH_SHORT).show();
-                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                //Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
+
+        mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
+        int question=mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        mNextButton=(Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
+                int question=mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue=mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId=0;
+        if(userPressedTrue==answerIsTrue){
+            messageResId=R.string.correct_toast;
+        }else{
+            messageResId=R.string.incorrect_toast;
+        }
+        Toast.makeText(QuizActivity.this,messageResId,Toast.LENGTH_SHORT).show();
     }
 }
